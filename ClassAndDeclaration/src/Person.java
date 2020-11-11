5314332a90b2e1ef28b7bd59bf78a79ed4913d06
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-11-11 21:30:48 +0530

    an example for default constructor, closes #5

--- ClassAndDeclaration/src/Person.java
+++ ClassAndDeclaration/src/Person.java
@@ -1,5 +1,5 @@
 public class Person {
-// this is an example for default constructor
+// this is an example for default constructor and parameterized constructor
     private long id;
     private String firstName;
     private String lastName;
@@ -11,6 +11,19 @@ public class Person {
         firstName = "defaultFirstName";
         lastName = "defaultLastName";
         gender = 'z';
+    }
+    public Person(long id){
+        this.id = id;
+    }
+    public Person(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public Person(long id, String firstName, String lastName, char gender){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
     }
     public static void main(String[] args) {
         System.out.println("Person");
@@ -36,6 +49,27 @@ public class Person {
         System.out.println(p2.firstName);
         System.out.println(p2.lastName);
         System.out.println(p2.gender);
+
+        Person p3 = new Person(963258741);
+        System.out.println("Person object p3");
+        System.out.println(p3.id);
+        System.out.println(p3.firstName);
+        System.out.println(p3.lastName);
+        System.out.println(p3.gender);
+
+        Person p4 = new Person("Java","SE8");
+        System.out.println("Person object p4");
+        System.out.println(p4.id);
+        System.out.println(p4.firstName);
+        System.out.println(p4.lastName);
+        System.out.println(p4.gender);
+
+        Person p5 = new Person(25986314,"firstPerson","secondPerson",'M');
+        System.out.println("Person object p5");
+        System.out.println(p5.id);
+        System.out.println(p5.firstName);
+        System.out.println(p5.lastName);
+        System.out.println(p5.gender);
     }
 
 }
