a94fdf6fbc2cc28988c4f9896528ffe6528c6fba
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-11-24 20:51:09 +0530

    Program: reverse the number and string, closes #18

---
+++ JavaSE8/src/com/javase8/programs/ReverseANumberAndAString.java
@@ -0,0 +1,59 @@
+package com.javase8.programs;
+
+public class ReverseANumberAndAString {
+
+    public static void main(String[] args) {
+        System.out.println("ReverseANumberAndAString");
+        System.out.println(reverseANumber(9876)); //6789
+        //System.out.println(reverseANumber(1234567890)); //0987654321 // zero is ignored
+        System.out.println(reverseAString1("Java"));
+        System.out.println(reverseAString1("9876")); // 6789
+        System.out.println(reverseAString1("1234567890")); //0987654321 // zero is not ignored
+        System.out.println(reverseAString1("{}()[]:',.<>?/"));
+//        System.out.println(reverseAString2("Java"));
+//        System.out.println(reverseAString2("9876"));
+//        System.out.println(reverseAString2("1234567890"));
+        System.out.println(reverseAString3("Java"));
+        System.out.println(reverseAString3("9876")); // 6789
+        System.out.println(reverseAString3("1234567890")); //0987654321 // zero is not ignored
+        System.out.println(reverseAString3("{}()[]:',.<>?/"));
+    }
+
+    private static String reverseAString3(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        return sb.reverse().toString();
+    }
+
+    private static String reverseAString1(String s) {
+
+        char[] chars = s.toCharArray();
+        int l = s.length();
+        String reverse = "";
+        for (int i = l - 1; i >= 0; i--) {
+            reverse = reverse + chars[i];
+        }
+        return reverse;
+    }
+
+    private static String reverseAString2(String s) {
+        // Incomplete
+        byte[] bytes = s.getBytes();
+        int l = s.length();
+        String reverse = "";
+        for (int i = l - 1; i >= 0; i--) {
+            reverse = reverse + bytes[i];
+        }
+        return reverse;
+    }
+
+    private static long reverseANumber(long n) {
+        long r = 0;
+        while (n > 0) {
+            r = r * 10 + n % 10;
+            n = n / 10;
+        }
+        return r;
+    }
+
+
+}
