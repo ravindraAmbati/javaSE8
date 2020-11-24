f2b9b0a0b67824a26d4988ad5dfe27622a2013c5
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-11-24 21:02:25 +0530

    Program: first N or first 100 prime numbers, closes #22

--- JavaSE8/src/com/javase8/programs/NumberNature.java
+++ JavaSE8/src/com/javase8/programs/NumberNature.java
@@ -15,7 +15,7 @@ public class NumberNature {
         System.out.println(isPrime(997));
     }
 
-    private static boolean isPrime(int n) {
+    public static boolean isPrime(int n) {
         if (n == 2 || n == 3) {
             return true;
         }
@@ -24,7 +24,7 @@ public class NumberNature {
         }
         for (int i = 3; i <= n / 2; i++) {
             if (n % i == 0) {
-                System.out.println("given " + n + " is divisible by " + i);
+                //System.out.println("given " + n + " is divisible by " + i); // doc
                 return false;
             }
         }
