c8b6e8dc811c50478da803bdb98ad004ff2bfb54
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-12-11 21:24:57 +0530

    Threading, closes #42

---
+++ JavaSE8/src/com/javase8/threading/example/FirstThread.java
@@ -0,0 +1,21 @@
+package com.javase8.threading.example;
+
+public class FirstThread extends Thread {
+
+    private static void m4() {
+        long start = System.currentTimeMillis();
+        System.out.println("method m4 is running on " + Thread.currentThread().getName() + " thread");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Total time taken: " + (end - start));
+    }
+
+    @Override
+    public void run() {
+        m4();
+    }
+}
